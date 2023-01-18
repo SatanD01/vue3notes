87bac050fe84005ca1e68622c80c17db56ddcb7e
Doston <gameplayer140701@gmail.com>
2023-01-18 12:20:21 +0500

    added tags

--- src/components/Notes/TheForm.vue
+++ src/components/Notes/TheForm.vue
@@ -2,23 +2,31 @@
   <div class="note-form-wrapper">
     <form class="note-form" @submit.prevent="onSubmit">
       <textarea required v-model="value" placeholder="Type your note"/>
+      <TheTagsList :items="tags" @onItemClick="handleClick"/>
       <button class="btn btnPrimary" type="submit">Add new Note</button>
     </form>
   </div>
 </template>
 
 <script>
+import TheTagsList from "@/components/UI/TheTagsList";
+
 export default {
   name: "TheForm",
+  components: {TheTagsList},
   data() {
     return {
-      value: ''
+      value: '',
+      tags: ['home', 'work', 'travel']
     }
   },
   methods: {
     onSubmit() {
       this.$emit('onSubmit', this.value);
       this.value = '';
+    },
+    handleClick(tag) {
+      console.log(tag)
     }
   }
 }
@@ -30,10 +38,15 @@ export default {
   flex-direction: column;
   align-items: center;
 }
+
 .note-form {
   display: flex;
   flex-direction: column;
   max-width: 600px;
   width: 100%;
+
+  textarea {
+    margin-bottom: 0;
+  }
 }
 </style>
---
+++ src/components/UI/TheTagsList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tags-list">
+    <div class="tag-item" v-for="tag in items" :key="tag" @click="$emit('onItemClick', tag)">
+      <span>
+        {{ tag }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheTagsList",
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.tags-list {
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+}
+.tag-item {
+  padding: 8px 22px;
+  margin-right: 10px;
+  background-color: white;
+  border-radius: 22px;
+  user-select: none;
+  cursor: pointer;
+  &:last-child {
+    margin-right: 0;
+  }
+}
+</style>
